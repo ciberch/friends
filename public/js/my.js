3e33f8ef42aa33d990ac2338ff30d6e8aecd43e3
ciberch <monica.keller@gmail.com>
2012-04-15 10:03:57 -0700

    Merge invite branch with my changes for styling and app id

--- public/js/my.js
+++ public/js/my.js
@@ -1,6 +1,6 @@
  window.fbAsyncInit = function() {
       FB.init({
-        appId      : '316154225118670', // App ID
+        appId      : app_id, // App ID
         channelUrl : '//friends.cloudfoundry.com/channel.html', // Channel File
         status     : true, // check login status
         cookie     : true, // enable cookies to allow the server to access the session
@@ -18,8 +18,8 @@
        js.src = "//connect.facebook.net/en_US/all.js";
        ref.parentNode.insertBefore(js, ref);
      }(document));
-      
-    
+
+
 
     $(".Recommend").on("click",function(event){
       console.log($(this).attr("uid"));
@@ -27,7 +27,7 @@
       console.log(user_id);
       FB.ui({method: 'apprequests',
       message: 'Find the right job here',
-      to: user_id, 
+      to: user_id,
       }, requestCallback);
     });
 
